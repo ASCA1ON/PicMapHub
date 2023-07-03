369fd221d5b4048d6b43c57c539e6fbe35810214
ASCA1ON <akash2468.ab@gmail.com>
2023-07-03 20:02:34 +0530

    routes added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,18 @@
-import React from 'react';
-
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, Navigate} from "react-router-dom";
+import Users from "./user/pages/Users";
+import NewPlaces from "./places/pages/NewPlaces";
 function App() {
   return (
-    <h1>starting</h1>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Users />}/>
+        <Route path="/new" element={<NewPlaces />}/>
+        <Route path="/*" element={<Navigate to="/" replace/>} />
+      </Routes>
+    </Router>
   );
-}
+  }
+
 
 export default App;
