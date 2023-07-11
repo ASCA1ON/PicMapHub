b6bb53e55b8a7cee387f3613440cfdc2d8bf2743
ASCA1ON <akash2468.ab@gmail.com>
2023-07-11 21:08:24 +0530

    auth pages done

--- frontend/src/user/pages/Auth.jsx
+++ frontend/src/user/pages/Auth.jsx
@@ -1,10 +1,93 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 
 import './Auth.css';
+import Input from '../../shared/components/FormElements/Input';
+import { VALIDATOR_EMAIL, VALIDATOR_MINLENGTH, VALIDATOR_REQUIRE, } from '../../shared/util/validators';
+import Button from '../../shared/components/FormElements/Button';
+import Card from '../../shared/components/UIElements/Card';
+import { useForm } from '../../shared/hooks/form-hook';
 
 const Auth = () => {
-  
+
+    const [isLogin, setIsLogin] = useState(true)
+
+    const [formState, inputHandler, setFormData] =useForm({
+        email:{
+            value:"",
+            isValid:false
+        },
+        password:{
+            value:"",
+            isValid:false
+        }
+    },false);
+
+    const authSubmitHandler = event =>{
+        event.preventDefault();
+        console.log(formState)
+    }
+    const switchHandler = () =>{
+        if (!isLogin) {
+            setFormData(
+              {
+                ...formState.inputs,
+                  name: undefined
+              },
+              formState.inputs.email.isValid && formState.inputs.password.isValid
+            );
+          } else { 
+            setFormData({
+                ...formState.inputs,
+                name:{
+                    value:'',
+                    isValid:false
+                }
+            }, false);
+          }
+        
+        setIsLogin(prev=>!prev)
+    }
+
+
+  return <Card className='authentication'>
+    <h2>Log in to PicMapHub</h2>
+    <hr />
+  <form className="place-for" onSubmit={authSubmitHandler}>
+    {!isLogin && <Input 
+    id= 'name'
+    type='name'
+    element='input'
+    placeholder='Enter your name'
+    label ="Name"
+    validators={[VALIDATOR_REQUIRE()]}
+    errorText="Please enter a Name"
+    onInput={inputHandler}
+    />}
+    <Input 
+    id= 'email'
+    type='email'
+    element='input'
+    placeholder='Enter your email'
+    label ="Email"
+    validators={[VALIDATOR_EMAIL()]}
+    errorText="Please enter valid email"
+    onInput={inputHandler}
+    />
+    <Input 
+    id= 'password'
+    type='password'
+    element='input'
+    placeholder='Enter your password'
+    label ="Password"
+    validators={[ VALIDATOR_MINLENGTH(5)]}
+    errorText="Please enter atleast 5 character password"
+    onInput={inputHandler}
+    />
+    <Button type='submit' disabled={!formState.isValid}>{isLogin?'Log in':'Sign up'}</Button>
+  </form>
+  <Button inverse onClick={switchHandler}>{!isLogin?'Log in':'Sign up'} on PicMapHub</Button>
+  </Card>
 };
 
 export default Auth;
